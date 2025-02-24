95ffc1b8794646251ac2cff69e4acf8cee3ccb91
MoodEasy <support@moodeasy.com>
2025-02-24 16:50:14 +0100

    Increase version to 1.0.5 (2025022400)

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_meccertbulkdownload';  // To check on upgrade, that module sits in correct place.
-$plugin->version   = 2025022000;        // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2025022400;        // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019052000;        // Requires this Moodle version (3.7.0).
-$plugin->release   = 'v1.0.4';
+$plugin->release   = 'v1.0.5';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [
     'mod_customcert' => 2019052005,  // Requires the "Custom certificate" plugin.
