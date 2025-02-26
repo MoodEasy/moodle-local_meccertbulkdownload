b2445e129123d6d2b7641f976b0f728ce47a3ab7
MoodEasy <support@moodeasy.com>
2025-02-26 17:40:29 +0100

    Add incompatibility to Moodle 405 and subsequent

--- version.php
+++ version.php
@@ -28,6 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_meccertbulkdownload';  // To check on upgrade, that module sits in correct place.
 $plugin->version   = 2025022400;        // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019052000;        // Requires this Moodle version (3.7.0).
+$plugin->incompatible = 405;
 $plugin->release   = 'v1.0.5';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [
